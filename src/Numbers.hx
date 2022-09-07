1b02fb358a314097ccb8bfef9ef400c805d4d197
kompasim <kompasim@ziipin.com>
2022-09-07 21:57:31 +0800

    update numbers

--- src/Numbers.hx
+++ src/Numbers.hx
@@ -5,6 +5,10 @@ class Numbers {
     private static var valid : Bool = false;
     private static var COMPLETE : String = "پۈتۈن";
     private static var ZERO : String = "نۆل";
+    private static var TEN : String = "ئون";
+    private static var TEN_SUFFIX : String = "دا";
+    private static var HUNDRED : String = "يۈز";
+    private static var HUNDRED_SUFFIX : String = "دە";
     private static var NAMES_1 : Array<String> = [
         "بىر",
         "ئىككى",
@@ -28,7 +32,6 @@ class Numbers {
         "توقسان",
     ];
     private static var NAMES_N : Array<String> = [
-        "يۈز",
         "مىڭ",
         "مىليون",
         "مىليارد",
@@ -36,7 +39,6 @@ class Numbers {
         "تىرىليارد"
     ];
     private static var NAMES_N_SUFFIX : Array<String> = [
-        "دە",
         "دا",
         "دا",
         "تا",
@@ -56,7 +58,7 @@ class Numbers {
     }
 
     private static function getHundredTimes(text : String, num : Int) : String {
-        return (text.length == 0) ? text : text + " " + NAMES_N[0];
+        return (text.length == 0) ? text : text + " " + HUNDRED;
     }
 
     private static function getHighTimes(text : String, times : Int) : String {
@@ -67,16 +69,16 @@ class Numbers {
         switch (times) {
             case 2: return getTenTimes(num);
             case 3: return getHundredTimes(text, num);
-            case 4: return getHighTimes(text, 1);
+            case 4: return getHighTimes(text, 0);
             case 5: return getTenTimes(num);
             case 6: return getHundredTimes(text, num);
-            case 7: return getHighTimes(text, 2);
+            case 7: return getHighTimes(text, 1);
             case 8: return getTenTimes(num);
             case 9: return getHundredTimes(text, num);
-            case 10: return getHighTimes(text, 3);
+            case 10: return getHighTimes(text, 2);
             case 11: return getTenTimes(num);
             case 12: return getHundredTimes(text, num);
-            case 13: return getHighTimes(text, 4);
+            case 13: return getHighTimes(text, 3);
             case 14: return getTenTimes(num);
             case 15: return getHundredTimes(text, num);
             case 16: return getHighTimes(text, 5);
@@ -86,7 +88,7 @@ class Numbers {
         }
     }
 
-    private static function process(num : Int) : String {
+    private static function processRead(num : Int) : String {
         if (num <= 0) {
             return ZERO;
         }
@@ -111,6 +113,11 @@ class Numbers {
 
 
     private static function appendFloatSuffix(text : String) : String {
+        if (StringTools.endsWith(text, TEN)) {
+            text += TEN_SUFFIX;
+        } else if (StringTools.endsWith(text, HUNDRED)) {
+            text += HUNDRED_SUFFIX;
+        }
         for (k => v in NAMES_N) {
             if (StringTools.endsWith(text, v)) {
                 text += NAMES_N_SUFFIX[k];
@@ -135,34 +142,125 @@ class Numbers {
         var nums = dot.split(Std.string(num));
         if (nums.length == 1) {
             var num1 : Int = Std.parseInt(nums[0]);
-            return process(num1);
+            return processRead(num1);
         } else {
             var num1 : Int = Std.parseInt(nums[0]);
             var num2 : Int = Std.parseInt(nums[1]);
-            var numx : Int = 10;
-            while (numx < num2) {
-                numx *= 10;
-            }
-            var text1 = process(num1);
-            var text2 = process(num2);
-            var textX = process(numx);
+            var numx : Int = Std.int(Math.pow(10, nums[1].length));
+            var text1 = processRead(num1);
+            var text2 = processRead(num2);
+            var textX = processRead(numx);
             trace("numx", numx, textX);
             return text1 + " " + COMPLETE + " " + appendFloatSuffix(textX) + " " + text2;
         }
         return "";
     }
 
+    public static function processWrite(words: Array<String>) : String {
+        var result = 0;
+        var length = words.length;
+        var power = 1;
+        var times = 1;
+        if (length == 0) {
+            return "";
+        }
+        while (length > 0) {
+            length--;
+            var word = words[length];
+            //
+            var isPower = false;
+            for (k => v in NAMES_N) {
+                if (v == word) {
+                    power = Std.int(Math.pow(10, (k + 1) * 3));
+                    times = 1;
+                    isPower = true;
+                    break;
+                }
+            }
+            if (isPower) {
+                continue;
+            }
+            if (word == HUNDRED) {
+                times = 100;
+                continue;
+            }
+            //
+            var num = 0;
+            for(k => v in NAMES_1) {
+                if (v == word) {
+                    num = k + 1;
+                }
+            }
+            for(k => v in NAMES_10) {
+                if (v == word) {
+                    num = k + 1;
+                    times = 10;
+                }
+            }
+            //
+            result += Std.int(num * power * times);
+        }
+        return Std.string(result);
+    }
+
+    
+    public static function write(text: String) : String {
+        var r = ~/\s+/gi;
+        var words = r.split(text);
+        var dotIndex = -1;
+        for(k => v in words) {
+            if (v == COMPLETE) {
+                dotIndex = k;
+                break;
+            }
+        }
+        var endIndex = dotIndex + 1;
+        var parseWord = words[dotIndex + 1];
+        var power = 0;
+        if (StringTools.startsWith(parseWord, NAMES_10[0])) {
+            power = 1;
+            if (!StringTools.endsWith(parseWord, NAMES_N_SUFFIX[0])) {
+                endIndex = dotIndex + 1 + 1;
+                parseWord = words[endIndex];
+            }
+        } else if (StringTools.startsWith(parseWord, HUNDRED)) {
+            power = 2;
+            if (!StringTools.endsWith(parseWord, HUNDRED_SUFFIX)) {
+                endIndex = dotIndex + 1 + 1;
+                parseWord = words[endIndex];
+            }
+        }
+        if (power == 0 || endIndex > dotIndex + 1) {
+            for(k => v in NAMES_N) {
+                if (StringTools.startsWith(parseWord, v)) {
+                    power += Std.int((k + 1) * 3);
+                }
+            }
+        }
+        if (dotIndex < 0 || power < 1) {
+            return processWrite(words);
+        } else {
+            var left = processWrite(words.slice(0, dotIndex));
+            var right = processWrite(words.slice(endIndex + 1, words.length));
+            var temp = StringTools.lpad(right, "0", power); 
+            return left + "." + temp;
+        }
+    }
+
     static function main() {
-        trace("numbers...");
-        var txt = "1234";
+        trace("read...");
+        // var txt = "0";
         // var txt = "12345678";
         // var txt = "100001";
-        var txt = "123.45";
+        var txt = "123.40110001";
         var r : String = read(txt);
         trace(r);
         #if sys
             sys.io.File.saveContent('output/test.txt', r);
         #end
+        trace("write...");
+        trace(write(r));
+
     }
 
 }
